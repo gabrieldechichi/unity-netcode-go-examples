ca2910247d26a398479eb328b047e319a550334e
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-12-17 16:54:46 -0300

    Separte run_win32 client and server init scripts

--- 2_GoldenPath/Cmdline/run_client.ps1
+++ 2_GoldenPath/Cmdline/run_client.ps1
@@ -1,5 +1,4 @@
 $current = Get-Location
 $buildPath = "${current}\Build\Win32\2_GoldenPath.exe"
 
-& $buildPath -mlapi server
 & $buildPath -mlapi client
---
+++ 2_GoldenPath/Cmdline/run_server.ps1
@@ -0,0 +1,4 @@
+$current = Get-Location
+$buildPath = "${current}\Build\Win32\2_GoldenPath.exe"
+
+& $buildPath -mlapi server
